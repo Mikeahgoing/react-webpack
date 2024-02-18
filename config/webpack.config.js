4e424dba3ec96c02f2ee594cc2c747cecc71d046
Mike <706420258@qq.com>
2024-02-18 19:02:15 +0800

    init

---
+++ config/webpack.config.js
@@ -0,0 +1,42 @@
+// webpack.config.js
+const path = require("path");
+// 引入插件
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const devConfig = require("./webpack.dev");
+const prodConfig = require("./webpack.prod");
+const { merge } = require("webpack-merge");
+
+module.exports = merge(
+  {
+    // 入口
+    entry: path.resolve(__dirname, "../src/index.jsx"),
+    // 模块
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-react"],
+            },
+          },
+        },
+        {
+          // 用正则去匹配要用该 loader 转换的 CSS 文件
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    // 插件
+    plugins: [
+      new HtmlWebpackPlugin({
+        // 指定模版
+        template: path.resolve(__dirname, "../public/index.html"),
+      }),
+    ],
+  },
+  process.env.NODE_ENV === "development" ? devConfig : prodConfig
+);

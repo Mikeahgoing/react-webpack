3b349bfec9862b898a470de9fb8c43b16825fbac
Mike <706420258@qq.com>
2024-02-21 18:11:44 +0800

    feat login page

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Home() {
   return (
-    <h1 className="text-primary text-4xl font-bold">
+    <h1 className="text-primary text-4xl font-bold border-solid border-2 border-sky-500">
       Hello world! I am using React
     </h1>
   );
--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Input, Tooltip } from "antd";
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="h-[300px] flex gap-3 flex-col border-solid border-2 border-sky-500 px-3 justify-start">
+        <div className="flex justify-center p-3 ">登录</div>
+        <Input
+          placeholder="Enter your username"
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          suffix={
+            <Tooltip title="输入用户名">
+              <InfoCircleOutlined
+                style={{
+                  color: "rgba(0,0,0,.45)",
+                }}
+              />
+            </Tooltip>
+          }
+        />
+        <Input.Password placeholder="input password" />
+        <Button type="primary" className="bg-red-400">登录</Button>
+      </div>
+    </div>
+  );
 }
